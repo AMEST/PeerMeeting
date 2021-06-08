c3e0def4808851a2795a999a6c5cfd2eb875f9a0
Klabukov Erik <amect@ya.ru>
2021-06-08 16:40:59 +0500

    Add comments

--- src/PeerMeeting.Host/Configuration/ConfigurationExtensions.cs
+++ src/PeerMeeting.Host/Configuration/ConfigurationExtensions.cs
@@ -2,8 +2,14 @@
 
 namespace PeerMeeting.Host.Configuration
 {
+    /// <summary>
+    /// Extensions methods for simple getting mapped configuration from appsettings
+    /// </summary>
     public static class ConfigurationExtensions
     {
+        /// <summary>
+        /// Get Redis Configuration 
+        /// </summary>
         public static RedisConfiguration GetRedisConfiguration(this IConfiguration configuration)
         {
             var redisConfiguration = new RedisConfiguration();
